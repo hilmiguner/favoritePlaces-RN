c7f03dcf3f722fc334e86f46161c777a5ad75b4a
Hilmi GUNER <hilmiguner@Hilmi-MacBook-Air.local>
2023-08-01 15:13:42 +0300

    commit

---
+++ components/Places/LocationPicker.js
@@ -0,0 +1,48 @@
+import { Alert, StyleSheet, View } from "react-native";
+import OutlinedButton from "../UI/OutlinedButton";
+import { Colors } from "../../constants/colors";
+
+import Geolocation from "@react-native-community/geolocation";
+import { useState } from "react";
+
+function LocationPicker() {
+    const [currentLocation, setCurrentLocation] = useState();
+
+    function getLocationHandler() {
+        Geolocation.getCurrentPosition(
+            (location) => {setCurrentLocation({ latitude: location.coords.latitude, longitude: location.coords.longitude })},
+            (error) => {Alert.alert("Error!", error.message)}
+        );
+    }
+
+    function pickOnMapHandler() {}
+
+    return(
+        <View>
+            <View style={styles.mapPreview}></View>
+            <View style={styles.actions}>
+                <OutlinedButton icon="location" onPress={getLocationHandler}>Locate User</OutlinedButton>
+                <OutlinedButton icon="map" onPress={pickOnMapHandler}>Pick on Map</OutlinedButton>
+            </View>
+        </View>
+    );
+}
+
+export default LocationPicker;
+
+const styles = StyleSheet.create({
+    mapPreview: {
+        width: "100%",
+        height: 200,
+        marginVertical: 8,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: Colors.primary100,
+        borderRadius: 4,
+    },
+    actions: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+    },
+});
