178c78ea6acdc470da533a48fb1b085a4c6773cc
Hilmi GUNER <hilmiguner@Hilmi-MacBook-Air.local>
2023-08-01 12:47:41 +0300

    commit

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,5 +1,53 @@
-function ImagePicker() {
+import { useState } from "react";
+import { View, Image, StyleSheet, Text } from "react-native";
 
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
+
+function ImagePicker() {
+    const [imageUri, setImageUri] = useState();
+
+    async function takeImageHandler() {
+        try {
+            const response = await launchCamera({
+                quality: 0.5,
+            });
+            setImageUri(response.assets[0].uri);
+        }
+        catch(error) {
+        }
+    }
+
+    let imagePreview = <Text>No image taken yet.</Text>;
+    if(imageUri) {
+        imagePreview = <Image style={styles.image} source={{ uri: imageUri }}/>;
+    }
+
+    return(
+        <View>
+            <View style={styles.imagePreview}>
+                {imagePreview}
+            </View>
+            <OutlinedButton icon="camera" onPress={takeImageHandler}>Take Image</OutlinedButton>
+        </View>
+    );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: "100%",
+        height: 200,
+        marginVertical: 8,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: Colors.primary100,
+        borderRadius: 4,
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+    },
+});
