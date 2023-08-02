2a6da2afdfd6be26af9a29ea63aec95e722473bc
Hilmi GUNER <hilmiguner@Hilmi-MacBook-Air.local>
2023-08-02 14:07:44 +0300

    commit

--- util/location.js
+++ util/location.js
@@ -1,6 +1,6 @@
 import { GOOGLE_MAP_API_KEY } from "../constants/api";
 
 export function getMapPreview(latitude, longitude) {
-    const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${latitude},${longitude}&key=${GOOGLE_MAP_API_KEY}`;
+    const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:null%7C${latitude},${longitude}&key=${GOOGLE_MAP_API_KEY}`;
     return imagePreviewUrl;
 }
